87c8a75a94d952d018d4fbb12c1cfb5343cbec20
Xeng Pha <69016286+phaxx031@users.noreply.github.com>
2020-10-28 18:47:43 -0500

    Add files via upload

--- index.js
+++ index.js
@@ -25,25 +25,21 @@ $(document).ready(function() {
   $('#shopping-item-toggle').on('click', function() {
     $('#shopping-item').toggleClass(shopping-item__checked, function() {
 
+      let buttonClicked = $(this);
+      buttonClicked.closest('li').find('.shopping-item').toggleClass('shopping-item__checked');
     });
-  });
 
-});
 
-//what i did above was basically if i click on the check button, it should toggle the class of crossing the name (shopping item checked) 
-//to current span class which is where the stuff like apple, oranges, etc. are, not sure how to do this though...
-
-$(document).ready(function() {
-
-  $('#shopping-item-delete').on('click', function() {
+  $('.shopping-item-delete').on('click', function() {
     $('ul').children().remove('selected');
   });
 
 });
 
-// putting delete instead of toggle breaks the very first jquery that make the add item button work for whatever reason...
-// my thinking is target the delete button and when we click it, it triggers an event where we delete the current li although
-// i am not totally sure how to target the current li... maybe use the this keyword - claudio
 
-// last assignment below is making sure enter/return works so we dont have to always click add item
+/* putting delete instead of toggle breaks the very first jquery that make the add item button work for whatever reason...
+/* my thinking is target the delete button and when we click it, it triggers an event where we delete the current li although
 
+/* i am not totally sure how to target the current li... maybe use the this keyword - claudio
+
+/* last assignment below is making sure enter/return works so we dont have to always click add item
